6f1445a1c2c5e37cae384195284253ab803e36a1
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-12-28 20:22:22 +0800

    Customization on my iMac

---
+++ personal/personal.el
@@ -0,0 +1,12 @@
+;;; personal.el --- Emacs Prelude: personal configuration
+
+;;; Commentary:
+
+;; manual configuration
+
+;;; Code:
+
+(disable-theme 'zenburn)
+(load-theme 'solarized-dark t)
+(provide 'personal)
+;;; personal.el ends here
